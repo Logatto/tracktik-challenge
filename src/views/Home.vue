5b36060f418a4ca0efdc08b4da4d7d6d6236794f
Wbeimar Logatto <wblomo22@gmail.com>
2021-04-25 00:18:53 -0500

    Implementation of material vuetify and first mockup app

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,178 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <v-text-field
+        label="Search site"
+        placeholder="Type to search a site"
+        outlined
+        dense
+        hide-details="auto"
+        class="my-2 mx-2"
+      >
+        <template v-slot:append-outer>
+          <v-icon >
+            mdi-magnify
+          </v-icon>
+          <v-menu>
+            <template v-slot:activator="{ on, attrs }">
+              <v-icon class="mx-2" v-bind="attrs" v-on="on">
+                mdi-filter
+              </v-icon>
+            </template>
+            <v-list>
+              <v-list-item>
+                <v-list-item-title>
+                  <v-icon>mdi-sort-alphabetical-ascending</v-icon>
+                </v-list-item-title>
+              </v-list-item>
+              <v-list-item>
+                <v-list-item-title>
+                  <v-icon>mdi-sort-alphabetical-descending</v-icon>
+                </v-list-item-title>
+              </v-list-item>
+            </v-list>
+          </v-menu>
+        </template>
+      </v-text-field>
+
+    <v-list three-line>
+      <template v-for="(item, index) in items">
+        <v-list-item
+          :key="item.name"
+          to="/site/1"
+        >
+          <v-list-item-avatar size="50" class="align-self-center">
+            <v-img :src="item.image"></v-img>
+          </v-list-item-avatar>
+
+          <v-list-item-content>
+            <v-list-item-title v-html="item.name"></v-list-item-title>
+            <v-list-item-subtitle v-html="item.address"></v-list-item-subtitle>
+            <v-list-item-subtitle v-html="item.contact"></v-list-item-subtitle>
+          </v-list-item-content>
+
+          <v-list-item-action class="align-self-center">
+            <v-icon
+              color="grey lighten-1"
+            >
+              mdi-chevron-right
+            </v-icon>
+          </v-list-item-action>
+        </v-list-item>
+        <v-divider :key="index"></v-divider>
+      </template>
+    </v-list>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+// import HelloWorld from '@/components/HelloWorld.vue';
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld,
+    // HelloWorld,
   },
+  data: () => ({
+    items: [
+      {
+        image: 'http://lorempixel.com/640/480/city',
+        name: 'Electronics HQ',
+        address: '611 Miller Glen',
+        contact: '533.880.9306 x0425',
+      },
+      {
+        image: 'http://lorempixel.com/640/480/city',
+        name: 'Electronics HQ1',
+        address: '611 Miller Glen',
+        contact: '533.880.9306 x0425',
+      },
+      {
+        image: 'http://lorempixel.com/640/480/city',
+        name: 'Electronics HQ2',
+        address: '611 Miller Glen',
+        contact: '533.880.9306 x0425',
+      },
+      {
+        image: 'http://lorempixel.com/640/480/city',
+        name: 'Electronics HQ3',
+        address: '611 Miller Glen',
+        contact: '533.880.9306 x0425',
+      },
+      {
+        image: 'http://lorempixel.com/640/480/city',
+        name: 'Electronics HQ',
+        address: '611 Miller Glen',
+        contact: '533.880.9306 x0425',
+      },
+      {
+        image: 'http://lorempixel.com/640/480/city',
+        name: 'Electronics HQ1',
+        address: '611 Miller Glen',
+        contact: '533.880.9306 x0425',
+      },
+      {
+        image: 'http://lorempixel.com/640/480/city',
+        name: 'Electronics HQ2',
+        address: '611 Miller Glen',
+        contact: '533.880.9306 x0425',
+      },
+      {
+        image: 'http://lorempixel.com/640/480/city',
+        name: 'Electronics HQ3',
+        address: '611 Miller Glen',
+        contact: '533.880.9306 x0425',
+      },
+      {
+        image: 'http://lorempixel.com/640/480/city',
+        name: 'Electronics HQ',
+        address: '611 Miller Glen',
+        contact: '533.880.9306 x0425',
+      },
+      {
+        image: 'http://lorempixel.com/640/480/city',
+        name: 'Electronics HQ1',
+        address: '611 Miller Glen',
+        contact: '533.880.9306 x0425',
+      },
+      {
+        image: 'http://lorempixel.com/640/480/city',
+        name: 'Electronics HQ2',
+        address: '611 Miller Glen',
+        contact: '533.880.9306 x0425',
+      },
+      {
+        image: 'http://lorempixel.com/640/480/city',
+        name: 'Electronics HQ3',
+        address: '611 Miller Glen',
+        contact: '533.880.9306 x0425',
+      },
+      {
+        image: 'http://lorempixel.com/640/480/city',
+        name: 'Electronics HQ',
+        address: '611 Miller Glen',
+        contact: '533.880.9306 x0425',
+      },
+      {
+        image: 'http://lorempixel.com/640/480/city',
+        name: 'Electronics HQ1',
+        address: '611 Miller Glen',
+        contact: '533.880.9306 x0425',
+      },
+      {
+        image: 'http://lorempixel.com/640/480/city',
+        name: 'Electronics HQ2',
+        address: '611 Miller Glen',
+        contact: '533.880.9306 x0425',
+      },
+      {
+        image: 'http://lorempixel.com/640/480/city',
+        name: 'Electronics HQ3',
+        address: '611 Miller Glen',
+        contact: '533.880.9306 x0425',
+      },
+    ],
+  }),
+  methods: {},
 };
 </script>
---
+++ src/views/SiteDetail.vue
@@ -0,0 +1,107 @@
+<template>
+  <v-card
+    class="mx-auto"
+  >
+    <v-list three-line class="blue" dark>
+      <template>
+        <v-list-item
+          :key="item.name"
+        >
+          <v-list-item-icon class="mr-4 align-self-center">
+            <v-icon medium @click="goBack">mdi-chevron-left</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-avatar size="50" class="mr-4 align-self-center">
+            <v-img :src="item.image"></v-img>
+          </v-list-item-avatar>
+
+          <v-list-item-content>
+            <v-list-item-title v-html="item.name"></v-list-item-title>
+            <v-list-item-subtitle v-html="item.address"></v-list-item-subtitle>
+            <v-list-item-subtitle v-html="item.contact"></v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
+      </template>
+    </v-list>
+
+    <v-list two-line>
+      <v-list-item>
+        <v-list-item-icon>
+          <v-icon color="blue">
+            mdi-account
+          </v-icon>
+        </v-list-item-icon>
+
+        <v-list-item-content>
+          <v-list-item-title>Hugh Windler</v-list-item-title>
+          <v-list-item-subtitle>National Applications Planner</v-list-item-subtitle>
+        </v-list-item-content>
+
+      </v-list-item>
+
+      <v-divider inset></v-divider>
+
+      <v-list-item>
+        <v-list-item-icon>
+          <v-icon color="blue">
+            mdi-phone
+          </v-icon>
+        </v-list-item-icon>
+
+        <v-list-item-content>
+          <v-list-item-title>(650) 555-1234</v-list-item-title>
+          <v-list-item-subtitle>Mobile</v-list-item-subtitle>
+        </v-list-item-content>
+
+      </v-list-item>
+
+      <v-divider inset></v-divider>
+
+      <v-list-item>
+        <v-list-item-icon>
+          <v-icon color="blue">
+            mdi-email
+          </v-icon>
+        </v-list-item-icon>
+
+        <v-list-item-content>
+          <v-list-item-title>aliconnors@example.com</v-list-item-title>
+          <v-list-item-subtitle>Personal</v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-divider inset></v-divider>
+
+      <v-list-item>
+        <v-list-item-icon>
+          <v-icon color="blue">
+            mdi-map-marker
+          </v-icon>
+        </v-list-item-icon>
+
+        <v-list-item-content>
+          <v-list-item-title>1400 Main Street</v-list-item-title>
+          <v-list-item-subtitle>Orlando, FL 79938</v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+    </v-list>
+  </v-card>
+</template>
+<script>
+export default {
+  name: 'SiteDetail',
+  data: () => ({
+    item: {
+      image: 'http://lorempixel.com/640/480/city',
+      name: 'Electronics HQ',
+      address: '611 Miller Glen',
+      contact: '533.880.9306 x0425',
+    },
+  }),
+  methods: {
+    goBack() {
+      return window.history.length > 1 ? this.$router.go(-1) : this.$router.push('/');
+    },
+  },
+};
+</script>
